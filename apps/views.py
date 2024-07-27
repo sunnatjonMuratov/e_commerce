9d8d0e41e8a0a5c6569e57d5b36487c9c6efc63c
Sunnatjon <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-07-27 22:30:38 +0500

    Update views.py

--- apps/views.py
+++ apps/views.py
@@ -1,5 +1,21 @@
-from django.shortcuts import render
+# views.py in your products app
+from django.shortcuts import render, redirect
+from .models import Product
+from .forms import ProductForm
+from .utils import remove_last_character
 
-
-def index(request):
-    return render(request, 'index.html')
+def edit_product(request, product_id):
+    product = Product.objects.get(id=product_id)
+    
+    if request.method == 'POST':
+        form = ProductForm(request.POST, instance=product)
+        if form.is_valid():
+            product = form.save(commit=False)
+            # Remove the last character from the product name
+            product.name = remove_last_character(product.name)
+            product.save()
+            return redirect('product_detail', product_id=product.id)
+    else:
+        form = ProductForm(instance=product)
+    
+    return render(request, 'products/edit_product.html', {'form': form})
