ddb2d11d85aae3cff6c2cc8586dde949c9fa380a
Sunnatjon Muratov <166012772+sunnatjonMuratov@users.noreply.github.com>
2024-08-23 10:41:34 +0500

    Update models.py

--- apps/models.py
+++ apps/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
